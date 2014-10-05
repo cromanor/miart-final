9c5040452a86e864b80cf749ed022bdc576b6fbb
cromanor <miart@claudios-imac.attlocal.net>
2014-10-05 12:47:55 -0400

    minor changes in css for map and set center

--- v2/js/controllers/controller.js
+++ v2/js/controllers/controller.js
@@ -4,10 +4,10 @@ miartApp.controller('MapCtrl', function ($scope) {
 	
   $scope.map = {
     center: {
-        latitude: 45,
-        longitude: -73
+        latitude: 25.796997,
+        longitude:  -80.189536
     },
-    zoom: 8
+    zoom: 14
 };
 
 });
