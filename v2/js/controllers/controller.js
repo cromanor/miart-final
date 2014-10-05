d9573a09d054736ad16fad0f5416046daf6afcbb
cromanor <miart@claudios-imac.attlocal.net>
2014-10-05 09:59:59 -0400

    Setting up

--- v2/js/controllers/controller.js
+++ v2/js/controllers/controller.js
@@ -1,1 +1,12 @@
-controller.js
+var miartApp = angular.module('miart-app', []);
+
+miartApp.controller('MapCtrl', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
