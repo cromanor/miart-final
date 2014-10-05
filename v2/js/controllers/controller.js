1a2afd4ee072c6a3eeb3150d26a07ec7caaed162
Brian <bgarb002@fiu.edu>
2014-10-05 12:38:22 -0400

    minotr

--- v2/js/controllers/controller.js
+++ v2/js/controllers/controller.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-define(["../angular.min"], function(angular) {
-	var miartApp = angular.module('miartApp', []);
-    console.log('In controller.js');
-	miartApp.controller('MapCtrl', function ($scope) {
-	  $scope.phones = [
-	    {'name': 'Nexus S',
-	     'snippet': 'Fast just got faster with Nexus S.'},
-	    {'name': 'Motorola XOOM™ with Wi-Fi',
-	     'snippet': 'The Next, Next Generation tablet.'},
-	    {'name': 'MOTOROLA XOOM™',
-	     'snippet': 'The Next, Next Generation tablet.'}
-	  ];
-	});
-=======
 var miartApp = angular.module('miartApp', ['google-maps']);
 
 miartApp.controller('MapCtrl', function ($scope) {
@@ -26,12 +11,10 @@ miartApp.controller('MapCtrl', function ($scope) {
   ];
 
   $scope.map = {
-    center: {
-        latitude: 45,
-        longitude: -73
-    },
-    zoom: 8
-};
-  
->>>>>>> 97cb4f903fe37ee925d579148b32bae750ed5171
+	    center: {
+	        latitude: 45,
+	        longitude: -73
+	    },
+	    zoom: 8
+	};
 });
