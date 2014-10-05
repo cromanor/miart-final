dd64578a74cbb4fcaf8eb2cf4e7fec4e8fccfbf6
cromanor <miart@claudios-imac.attlocal.net>
2014-10-05 12:37:09 -0400

    fix conflicts

--- v2/js/controllers/controller.js
+++ v2/js/controllers/controller.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-define(["../angular.min"], function(angular) {
-	var miartApp = angular.module('miartApp', []);
-    console.log('In controller.js');
-	miartApp.controller('MapCtrl', function ($scope) {
-	  $scope.phones = [
-	    {'name': 'Nexus S',
-	     'snippet': 'Fast just got faster with Nexus S.'},
-	    {'name': 'Motorola XOOM™ with Wi-Fi',
-	     'snippet': 'The Next, Next Generation tablet.'},
-	    {'name': 'MOTOROLA XOOM™',
-	     'snippet': 'The Next, Next Generation tablet.'}
-	  ];
-	});
-=======
 var miartApp = angular.module('miartApp', ['google-maps']);
 
 miartApp.controller('MapCtrl', function ($scope) {
@@ -32,6 +17,5 @@ miartApp.controller('MapCtrl', function ($scope) {
     },
     zoom: 8
 };
-  
->>>>>>> 97cb4f903fe37ee925d579148b32bae750ed5171
+
 });
