6d05dc21544b87ce6e154b24b0c8b29d6202253c
cromanor <miart@claudios-imac.attlocal.net>
2014-10-05 12:42:38 -0400

    cleaned up controller.js

--- v2/js/controllers/controller.js
+++ v2/js/controllers/controller.js
@@ -1,15 +1,7 @@
 var miartApp = angular.module('miartApp', ['google-maps']);
 
 miartApp.controller('MapCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
-
+	
   $scope.map = {
     center: {
         latitude: 45,
